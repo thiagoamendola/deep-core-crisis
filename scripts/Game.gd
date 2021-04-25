a6c1bae9b7deb80c691516240d647bd91dc7e182
Thiago Amendola <amendolathiago@gmail.com>
2021-04-25 02:24:13 -0700

    Add genetic procedural maze generation and select 10 mazes for the game

---
+++ scripts/Game.gd
@@ -0,0 +1,58 @@
+extends Node2D
+
+# Constants
+
+const NUMBER_MAPS = 10
+const EVO_GENERATIONS_PER_MAPS = 10
+
+const MapData = preload("map_data.gd")
+const MapEvaluator = preload("map_evaluator.gd")
+const EvolutionManager = preload("evolution_manager.gd")
+
+const MapSpawner = preload("map_spawner.gd")
+
+onready var tile_map = get_node("TileMap")
+
+var evolution_manager
+var map_evaluator
+var maps_list
+
+var map_index
+
+func _ready():
+	
+	# Initial setup
+	
+	randomize()
+	maps_list = []
+	evolution_manager = EvolutionManager.new()
+	map_evaluator = MapEvaluator.new()
+	tile_map.get_node("Fitness").text = ""
+	
+	# Generate all maps for this run
+	
+	for i in range(NUMBER_MAPS):
+		evolution_manager.initiate_population()
+		
+		for j in range(EVO_GENERATIONS_PER_MAPS):
+			evolution_manager.update_generation()
+		
+		maps_list.append(evolution_manager.map_array[0])
+		print(str(map_evaluator.get_fitness(evolution_manager.map_array[0])))
+	
+	map_index = 0
+	render_map(maps_list[map_index])
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_T:
+			map_index += 1
+			render_map(maps_list[map_index])
+
+func render_map(map):
+	# Get a matrix representation of the map.
+	var matrix = MapSpawner.get_map_matrix_border(map)
+	# Assign matrix to map view.
+	for i in range(map.SIZE.x+2):
+		for j in range(map.SIZE.y+2):
+			tile_map.set_cellv(Vector2(i,j), matrix[i][j])
---
+++ scripts/map_data.gd
@@ -0,0 +1,109 @@
+extends Node
+
+# Imports
+
+const utils = preload("utils.gd")
+
+# Constants
+
+const SIZE = Vector2(24,16)
+const ROOM_SIZE_MEAN = 6
+const ROOM_SIZE_DEVIATION = 1
+const NUMBER_ROOMS_MEAN = 5
+const NUMBER_ROOMS_DEVIATION = 2
+const NUMBER_CORRIDORS_MEAN = 7
+const NUMBER_CORRIDORS_DEVIATION = 3
+
+# Structs
+
+class Room:
+	var init_pos
+	var size
+
+class Corridor:
+	var points
+
+
+var room_list
+var corridor_list
+var entrance
+var exit
+
+func _init():
+	room_list = []
+	corridor_list = []
+
+# Creation
+
+func create_room(init_pos, size):
+	var room = Room.new()
+	room.init_pos = init_pos
+	room.size = size
+	room_list.append(room)
+
+func create_corridor(points):
+	var corridor = Corridor.new()
+	corridor.points = points
+	corridor_list.append(corridor)
+
+# Random creation
+
+func create_random_map():
+	var num_rooms = utils.normal_limits(NUMBER_ROOMS_MEAN, NUMBER_ROOMS_DEVIATION, 2, NUMBER_ROOMS_MEAN+2*NUMBER_ROOMS_DEVIATION)
+	var num_corridors = utils.normal_limits(NUMBER_CORRIDORS_MEAN, NUMBER_CORRIDORS_DEVIATION, num_rooms-1, NUMBER_CORRIDORS_MEAN+2*NUMBER_CORRIDORS_DEVIATION)
+	for i in range(num_rooms):
+		create_random_room()
+	for i in range(num_corridors):
+		create_random_corridor()
+	create_random_entrance_exit()
+
+func create_random_room():
+	var room_size = Vector2(utils.normal_limits(ROOM_SIZE_MEAN, ROOM_SIZE_DEVIATION, 2, SIZE.x), utils.normal_limits(ROOM_SIZE_MEAN, ROOM_SIZE_DEVIATION, 2, SIZE.y))
+	var room_pos = Vector2(rand_range(0, SIZE.x-room_size.x), rand_range(0, SIZE.y-room_size.y))
+	create_room(room_pos, room_size)
+
+func create_random_corridor():
+	var num_points = utils.normal_limits(3, 1, 2, 6)
+	var points = []
+	var axis = int(rand_range(0,2))
+	var point
+	for i in range(num_points):
+		if i == 0 :
+			point = Vector2(int(rand_range(0, SIZE.x)), int(rand_range(0, SIZE.y)))
+			points.append(point)
+		else:
+			point = Vector2()
+			if axis == 0:
+				point.x = points[len(points)-1].x
+				point.y = int(rand_range(0, SIZE.y))
+			else:
+				point.x = int(rand_range(0, SIZE.x))
+				point.y = points[len(points)-1].y
+			axis = int(axis+1)%2
+			points.append(point)
+	create_corridor(points)
+
+func create_random_entrance_exit():
+	var available_rooms = room_list.duplicate()
+	available_rooms.shuffle()
+	var entrance_room = available_rooms.pop_back()
+	var exit_room = available_rooms.pop_back()
+	entrance = Vector2(rand_range(entrance_room.init_pos.x, entrance_room.init_pos.x + entrance_room.size.x), rand_range(entrance_room.init_pos.y, entrance_room.init_pos.y + entrance_room.size.y))
+	exit = Vector2(rand_range(exit_room.init_pos.x, exit_room.init_pos.x + exit_room.size.x), rand_range(exit_room.init_pos.y, exit_room.init_pos.y + exit_room.size.y))
+
+func create_random_position():
+	var room = room_list[rand_range(0, len(room_list))]
+	var random_pos = Vector2(int(rand_range(room.init_pos.x, room.init_pos.x + room.size.x)), int(rand_range(room.init_pos.y, room.init_pos.y + room.size.y)))
+	return random_pos
+
+
+# Others
+
+func print_map_data():
+	for corridor in corridor_list:
+		var text = "corridor: {"
+		for point in corridor.points:
+			text+="("+str(point.x)+", "+str(point.y)+")"
+		text+="}"
+		print(text)
+	pass
